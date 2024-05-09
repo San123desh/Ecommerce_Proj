f0cf96c096ad54fce0133518deaa678f52ccb81f
San123desh <suuchang53@gmail.com>
2024-05-09 21:47:20 +0545

    logo

--- ecomweb/base/emails.py
+++ ecomweb/base/emails.py
@@ -1,4 +1,4 @@
-
+import imp
 from django.conf import settings
 from django.core.mail import send_mail
 
@@ -6,7 +6,7 @@ from django.core.mail import send_mail
 
 
 def send_account_activation_email(email , email_token):
-    return
+    # return
     subject = 'Your account needs to be verified'
     email_from = settings.EMAIL_HOST_USER
     message = f'Hi, click on the link to activate your account http://127.0.0.1:8000/accounts/activate/{email_token}'
