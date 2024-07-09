90fd96a53f0533a8d49eca0ca333ce41fecf4910
San123desh <suuchang53@gmail.com>
2024-07-09 13:39:43 +0545

    erase active_email

--- ecomweb/accounts/urls.py
+++ ecomweb/accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from accounts.views import login_page,register_page , activate_email,add_to_cart, cart, remove_cart, remove_coupon, checkout, generate_invoice
+from accounts.views import login_page,register_page ,add_to_cart, cart, remove_cart, remove_coupon, checkout, generate_invoice
 # from products.views import add_to_cart
 
 urlpatterns = [
    path('login/' , login_page , name="login" ),
    path('register/' , register_page , name="register"),
-   path('activate/<email_token>/' , activate_email , name="activate_email"),
+   # path('activate/<email_token>/' , activate_email , name="activate_email"),
    path('cart/' , cart , name="cart"),
    path('add-to-cart/<uid>/',add_to_cart , name="add_to_cart"),
    path('remove-cart/<cart_item_uid>/',remove_cart , name="remove_cart"),
